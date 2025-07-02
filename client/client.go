fa2189f7a0937ea910b9a9edf6a25ffd2a1e068e
BenPfeiffer-TX <ben.pfeiffer97@gmail.com>
2025-07-01 22:58:25 -0500

    alot of updates to client, basic implementation of chat being written to list that dynamically updates in ui

--- client/client.go
+++ client/client.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"net"
 
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/container"
-	"fyne.io/fyne/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
 )
 
-func sendMessage(input *widget.Entry, conn net.Conn) {
+func sendMessage(input *widget.Entry, chatwindow binding.ExternalStringList, conn net.Conn) {
 	//input: pointer to widget.Entry, current connection
 	text := input.Text
 	_, err := conn.Write([]byte(text))
@@ -17,6 +19,7 @@ func sendMessage(input *widget.Entry, conn net.Conn) {
 		fmt.Println(err)
 		return
 	}
+	chatwindow.Append(text)
 	//blank out text field after sending
 	input.SetText("")
 	return
@@ -30,18 +33,37 @@ func main() {
 		return
 	}
 
+	chatlog := []string{}
+	chatwindow := binding.BindStringList(&chatlog)
+
 	//fyne app window stuff
 	a := app.New()
 	windowName := "SecuRoom chat client"
 	w := a.NewWindow(windowName)
 	message := widget.NewLabel(windowName) //change this to be IP of connected server?
 	input := widget.NewEntry()
+
+	chat := widget.NewListWithData(chatwindow,
+		func() fyne.CanvasObject { return widget.NewLabel("") },
+		func(i binding.DataItem, o fyne.CanvasObject) { o.(*widget.Label).Bind(i.(binding.String)) },
+	)
+	//constantly call chat.UnselectAll() in a goroutine so that no entries in the list can be highlighted
+	//probably a better way to handle this
+	go func() {
+		for {
+			fyne.Do(func() { chat.UnselectAll() })
+		}
+	}()
+
 	input.SetPlaceHolder("type here")
-	send := widget.NewButton("send", func() { sendMessage(input, conn) })
-	content := container.NewVBox(message, input, send)
+	send := widget.NewButton("send", func() { sendMessage(input, chatwindow, conn) })
+	content := container.NewBorder(message, input, nil, send, chat)
 	w.SetContent(content)
 	w.ShowAndRun()
-
+	//todo: key detection for pressing enter
+	//make a toolbar for connecting / disconnecting
+	//figure out container layout changes
+	//receive chat log from server
 	tidyUp(conn)
 }
 
